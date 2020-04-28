071b9a4694ac3907e0d2c5bc6bf1e34af7260054
Edin Mesanovic <1078090@ucn.dk>
2020-04-28 09:49:34 +0200

    Oprettelse af Review klassen og convert metoderne.

---
+++ WebClient/Model/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebshopClient.Model
+{
+    public class Review
+    {
+        public int Rating { get; set; }
+        public String Comment { get; set; }
+    }
+}
